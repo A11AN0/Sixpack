332343e21cb28ed42c22e7ff0af262b4b291e32f
Allan <matrixboston7613@gmail.com>
2022-01-28 20:49:54 +0800

    set up services file, successful fetch from brewdog api

--- src/services/beer.service.js
+++ src/services/beer.service.js
@@ -1,1 +1,6 @@
-  
+export const getBeers = async(filter)=>{
+    if(filter === 'none') return await ((await fetch('https://api.punkapi.com/v2/beers?page=1&per_page=80')).json())
+    if(filter === 'abv') return await (( await fetch('https://api.punkapi.com/v2/beers?abv_gt=6')).json())
+    if(filter === 'classic') return await ((await fetch('https://api.punkapi.com/v2/beers?brewed_before=01-2010')).json())
+    if(filter === 'acidity') return await (await ((await fetch('https://api.punkapi.com/v2/beers?page=1&per_page=80')).json())).filter((beer)=>beer.ph < 4)
+}
