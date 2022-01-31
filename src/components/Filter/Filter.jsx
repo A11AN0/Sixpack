8d53fb4203f5642a6f6c85243750b3472b6b1774
Allan <matrixboston7613@gmail.com>
2022-01-31 14:25:38 +0800

    navbar complete

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -9,15 +9,15 @@ const Filter = (props) => {
 
   return (
     <div className={styles.container}>
-      <div>
+      <div className={styles.customRadioButton}>
         <input type="radio" id="ABV" name="drone" value="ABV" onInput={()=>{determineBeerSelection('abv')}}/>
          <label>ABV {`>`} 6%</label>
       </div>
-      <div>
+      <div className={styles.customRadioButton}>
         <input type="radio" id="Classic" name="drone" value="Classic" onInput={()=>{determineBeerSelection('classic')}}/>
          <label >Classic Range</label>
       </div>
-      <div>
+      <div className={styles.customRadioButton}>
         <input type="radio" id="Acidity" name="drone" value="Acidity" onInput={()=>{determineBeerSelection('acidity')}}/>
          <label>Acidity</label>
       </div> 
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import styles from './SearchBar.module.scss'
 
 
+
 const SearchBar = (props) => {
   const {optimizeBeerSelection, determineBeerSelection, setDisplayFilter, displayFilter}=props
 
 
   return (
     <div className={styles.content}>
-      <input type="text" placeholder="Find a beer!" onInput={e => optimizeBeerSelection(e.target.value)} /> 
+      <input type="text" placeholder="FIND A BEER" onInput={e => optimizeBeerSelection(e.target.value)} /> 
       <div className={styles.filterButton} onClick={()=>{
         determineBeerSelection('none')
         setDisplayFilter(!displayFilter)
-        }}></div>
+        }}><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="filter" class="svg-inline--fa fa-filter fa-w-16" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M487.976 0H24.028C2.71 0-8.047 25.866 7.058 40.971L192 225.941V432c0 7.831 3.821 15.17 10.237 19.662l80 55.98C298.02 518.69 320 507.493 320 487.98V225.941l184.947-184.97C520.021 25.896 509.338 0 487.976 0z"/></svg></div>
     </div>
   )
 }
