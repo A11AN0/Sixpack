5d6bab8b9648eae3a34454f36145d9d4508caa8a
Allan <matrixboston7613@gmail.com>
2022-02-01 15:47:38 +0800

    unit testing for searchbar component complete

---
+++ src/components/SearchBar/SearchBar.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import SearchBar from './SearchBar';
+
+
+describe("SearchBar tests", () => {
+  let component;
+  let mockFn
+
+  beforeEach(()=>{
+    mockFn = jest.fn();
+    component = shallow(<SearchBar optimizeBeerSelection={mockFn} determineBeerSelection={mockFn} setDisplayFilter={mockFn}/>);
+  })
+
+  it("should render", ()=>{
+    expect(component).toBeTruthy();
+  })
+
+  it("should call the parent function 'optimizeBeerSelection' on input", ()=>{
+    component.find("input").simulate("input", {target: {value: "User input"}} )
+    expect(mockFn).toHaveBeenCalled();
+  })
+
+  it("When the filter svg symbol is clicked, the toggleFilter() function should be called", ()=>{
+    expect(component.find('path').prop('fill')).toBe('black')
+    component.find('svg').simulate('click')
+    expect(component.find('path').prop('fill')).toBe('green')
+    component.find('svg').simulate('click')
+    expect(component.find('path').prop('fill')).toBe('black') 
+  })
+  
+})
