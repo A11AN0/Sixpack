707ebd2b42d07a81614288beead9c8b3e16a2dfe
Allan <matrixboston7613@gmail.com>
2022-02-01 14:11:15 +0800

    cardBack component unit testing complete

--- src/components/CardBack/CardBack.test.js
+++ src/components/CardBack/CardBack.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import CardBack from './CardBack';
+import beers from "../../data/beers";
 
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<CardBack />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe("CardBack tests", () => {
+  let component;
+  let testBeer;
+
+  beforeEach(()=>{
+    testBeer = beers[0]
+    component = shallow(<CardBack beer={testBeer}/>);
+  })
+
+  it("should render", ()=>{
+    expect(component).toBeTruthy();
+  })
+
+  it("should render the correct information, based on the beer passed in as props", ()=>{
+    const {name, first_brewed, ph, tagline} = testBeer;
+    expect(component.text()).toContain(name.toUpperCase());
+    expect(component.text()).toContain(first_brewed);
+    expect(component.text()).toContain(`${ph}`);
+    expect(component.text()).toContain(tagline.toUpperCase());
+  })
+
+  it("should render the correct image", ()=>{
+    const beerImage = testBeer.image_url;
+
+    expect(component.find( "img").prop("src")).toBe(beerImage);
+  })
+})
