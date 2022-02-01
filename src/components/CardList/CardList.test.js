0e330aa342abc24cd76a7ca1fd062366c6e77e83
Allan <matrixboston7613@gmail.com>
2022-02-01 14:27:00 +0800

    CardList component unit testing complete

--- src/components/CardList/CardList.test.js
+++ src/components/CardList/CardList.test.js
@@ -1,10 +1,24 @@
 import React from "react";
 import CardList from "./CardList";
+import Card from "../Card";
+import {shallow} from "enzyme";
+import beers from "../../data/beers";
 
 describe("CardList tests", () => {
   let component;
 
-  beforeEach(() => {
-    component =
+  beforeEach(()=>{
+    component = shallow(<CardList beers={beers}/>);
   })
-});
+
+  it("should render", ()=>{
+    expect(component).toBeTruthy();
+  })
+
+  it("should render the correct number of card components", () => {
+    const numberOfRecipes = beers.length;
+    const numberOfRenderedCards = component.find(Card).length;
+
+    expect(numberOfRecipes).toEqual(numberOfRenderedCards);
+  })
+})
