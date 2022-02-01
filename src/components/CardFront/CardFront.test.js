b1dbce26771229edaefb98338fb4e7e091daf8f0
Allan <matrixboston7613@gmail.com>
2022-02-01 13:44:06 +0800

    Card front component unit testing complete

--- src/components/CardFront/CardFront.test.js
+++ src/components/CardFront/CardFront.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import CardFront from './CardFront';
+import beers from "../../data/beers";
 
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<CardFront />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe("CardFront tests", () => {
+  let component;
+  let testBeer;
+
+  beforeEach(()=>{
+    testBeer = beers[0]
+    component = shallow(<CardFront beer={testBeer}/>);
+  })
+
+  it("should render", ()=>{
+    expect(component).toBeTruthy();
+  })
+
+  it("should render the correct name based on the recipe passed in as props", ()=>{
+    const beerName = testBeer.name;
+    expect(component.text()).toContain(beerName.toUpperCase());
+  })
+
+  it("should render the correct image", ()=>{
+    const beerImage = testBeer.image_url;
+
+    expect(component.find( "img").prop("src")).toBe(beerImage);
+  })
+})
