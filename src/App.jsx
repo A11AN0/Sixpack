bd8208d184c57142308a0493df1d8036251ecf04
Allan <matrixboston7613@gmail.com>
2022-02-19 18:50:09 +0800

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
-import CardList from './components/CardList';
-import {getBeers} from './services/beer.service';
-import styles from './App.module.scss'
-
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import CardList from "./components/CardList";
+import { getBeers } from "./services/beer.service";
+import styles from "./App.module.scss";
 
 const App = () => {
+  const [beers, setBeers] = useState([]);
+  const [filter, setFilter] = useState("none");
 
-const [beers, setBeers] = useState([]);
-const [filter, setFilter] = useState('none')
+  const determineBeerSelection = async (filterChoice) => {
+    setBeers(await getBeers(filterChoice));
+    setFilter(filterChoice);
+  };
 
-const determineBeerSelection = async (filterChoice)=>{
-  setBeers(await getBeers(filterChoice))
-  setFilter(filterChoice)
-}
+  const optimizeBeerSelection = async (input) => {
+    const currentArray = await getBeers(filter);
+    setBeers(
+      currentArray.filter((beer) =>
+        beer.name.toLowerCase().includes(input.toLowerCase())
+      )
+    );
+  };
 
-const optimizeBeerSelection = async (input)=>{
-  const currentArray = await getBeers(filter)
-  setBeers(currentArray.filter((beer)=>((beer.name).toLowerCase()).includes(input.toLowerCase())))
-}
-
-useEffect(()=>{
-  determineBeerSelection(filter)
-}, [])
-  
+  useEffect(() => {
+    determineBeerSelection(filter);
+  }, []);
 
   return (
     <>
-      <Navbar determineBeerSelection={determineBeerSelection} optimizeBeerSelection={optimizeBeerSelection}/> 
-      <CardList beers={beers} />  
+      <Navbar
+        determineBeerSelection={determineBeerSelection}
+        optimizeBeerSelection={optimizeBeerSelection}
+      />
+      <CardList beers={beers} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
